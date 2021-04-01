13ab7e85b90549038f9273d3d4253b1419e632ba
CeresBarros <cbarros@mail.ubc.ca>
2021-04-01 15:15:33 -0700

    P(sim)$fitRes used to calcualte rescaling factor

--- fireSense_dataPrep.R
+++ fireSense_dataPrep.R
@@ -351,8 +351,8 @@ prepFireSenseData <- function(sim) {
     }
 
     if (P(sim)$rescalePredictionObjs) {
-      ## rescaling factor calcualted as (newRes[1]/oldRes[1])^2
-      sim$rescaleFactor <- (res(sim$rasterToMatch)[1]/res(sim$weatherDataPred)[1])^2
+      ## rescaling factor calculated as (newRes[1]/oldRes[1])^2
+      sim$rescaleFactor <- (res(sim$rasterToMatch)[1]/P(sim)$fitRes)^2
 
       ## IgnitionPredict can use finer scale rasters, as long as predictions are rescaled (P(sim)$rescaleFactor)
       fuelTypesCoverPred <- projectInputs(fuelTypesCoverPred,
