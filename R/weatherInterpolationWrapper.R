6b9e307fa79efae6bc6e068d58a29278d802bdea
CeresBarros <cbarros@mail.ubc.ca>
2021-04-01 15:08:41 -0700

    years as names in weatherDataMDCStk

--- R/weatherInterpolationWrapper.R
+++ R/weatherInterpolationWrapper.R
@@ -5,15 +5,16 @@
 weatherInterpolationWrapper <- function(weatherDataMDC, RTMLLowRes, form) {
   if (class(form) != "formula")
     form <- as.formula(form)
-  weatherDataMDCStk <- lapply(unique(weatherDataMDC$year), FUN = function(yr) {
+  years <- c(unique(weatherDataMDC$year))
+  names(years) <- paste0("year_", years)
+  weatherDataMDCStk <- sapply(years, FUN = function(yr) {
     weatherSPDF <- as_Spatial(weatherDataMDC[weatherDataMDC$year == yr,])
     interpModel <- gstat(formula = form, data = weatherSPDF, set = list(idp = 0),
                          nmax = 8)   ## using 8 nearest neighbours
     weatherRas <- interpolate(object = RTMLLowRes, model = interpModel)  ## interpolate on RTM
     mask(weatherRas, RTMLLowRes)
-  })
+  }, simplify = FALSE, USE.NAMES = TRUE)
 
   weatherDataMDCStk <- raster::stack(weatherDataMDCStk)
-  names(weatherDataMDCStk) <- unique(weatherDataMDC$year)
   weatherDataMDCStk
 }
