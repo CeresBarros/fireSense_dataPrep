b2de1d5ece5aeb5b323317954bfabd595d9ec75f
CeresBarros <cbarros@mail.ubc.ca>
2021-06-14 13:46:16 -0700

    bugfix weather interpolations: interpolate on a data.frame
    * maybe due to PROJ changes, the predict.gstat call within interpolate was complaining about projection mismatches and newdata

--- R/weatherInterpolationWrapper.R
+++ R/weatherInterpolationWrapper.R
@@ -8,10 +8,13 @@ weatherInterpolationWrapper <- function(weatherDataMDC, rasterToMatch, form) {
   years <- c(unique(weatherDataMDC$year))
   names(years) <- paste0("year_", years)
   weatherDataMDCStk <- sapply(years, FUN = function(yr) {
-    weatherSPDF <- as_Spatial(weatherDataMDC[weatherDataMDC$year == yr,])
-    interpModel <- gstat(formula = form, data = weatherSPDF, set = list(idp = 0),
-                         nmax = 8)   ## using 8 nearest neighbours
-    weatherRas <- interpolate(object = rasterToMatch, model = interpModel)  ## interpolate on RTM
+    ## converting to a DF was necessary due to issues with spatial projection
+    ## interpolate was erroring even if predict.gstat was working.
+    weatherDF <- st_set_geometry(weatherDataMDC[weatherDataMDC$year == yr,], NULL)
+    weatherDF <- cbind(weatherDF, st_coordinates(weatherDataMDC[weatherDataMDC$year == yr,]))
+    interpModel <- gstat(formula = form, data = weatherDF, set = list(idp = 0),
+                         nmax = 8, locations = ~X+Y)   ## using 8 nearest neighbours
+    weatherRas <- interpolate(rasterToMatch, interpModel, xyNames = c('X', 'Y'))  ## interpolate on RTM
     mask(weatherRas, rasterToMatch)
   }, simplify = FALSE, USE.NAMES = TRUE)
 
