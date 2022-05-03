340bbb6646ebdcce59d2fa742015d0e7de466620
CeresBarros <cbarros@mail.ubc.ca>
2022-05-03 12:59:28 -0600

    finer control of parallelisation in fireSenseDataPrep

--- R/calcFuelCoverWrapper.R
+++ R/calcFuelCoverWrapper.R
@@ -5,15 +5,19 @@
 
 calcFuelCoverWrapper <- function(fuelTypesStk, RTMPolyGrid,
                                  parallel = TRUE, ...) {
-  if (parallel) {
     dots <- list(...)
     if (is.null(dots$strategy))
       dots$strategy <- multisession
     if (is.null(dots$gc))
       dots$gc <- TRUE
+    if (is(dots$strategy, "sequential") |
+        isTRUE(dots$workers == 1)) {
+      parallel <- FALSE
+    }
 
     dots$globals <- list(fuelTypesStk = fuelTypesStk, RTMPolyGrid = RTMPolyGrid)
 
+  if (parallel) {
     do.call(plan, dots)
     fuelTypesCover <- future_lapply(unstack(fuelTypesStk), FUN = function(ras) {
       exact_extract(ras, RTMPolyGrid, "count")
